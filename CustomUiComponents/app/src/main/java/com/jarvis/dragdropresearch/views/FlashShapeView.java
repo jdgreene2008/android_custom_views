7beb237be13e257e634e925db472bc5b644549ce
Jarvis Greene <jgreene@imobile3.com>
2019-10-17 21:27:31 -0400

    Extracted FlashShape draw methods to a DrawingUtils class.

--- CustomUiComponents/app/src/main/java/com/jarvis/dragdropresearch/views/FlashShapeView.java
+++ CustomUiComponents/app/src/main/java/com/jarvis/dragdropresearch/views/FlashShapeView.java
@@ -4,18 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
-import android.os.Build;
 import android.util.AttributeSet;
 
 import com.jarvis.dragdropresearch.funwithshapes.ArcShape;
 import com.jarvis.dragdropresearch.funwithshapes.FlashShape;
 import com.jarvis.dragdropresearch.funwithshapes.RectangleShape;
-import com.jarvis.dragdropresearch.funwithshapes.SpiralArcDescriptor;
-import com.jarvis.dragdropresearch.funwithshapes.SpiralSegment;
 import com.jarvis.dragdropresearch.funwithshapes.SpiralShape;
 import com.jarvis.dragdropresearch.funwithshapes.TriangleShape;
 import com.jarvis.dragdropresearch.interpolators.AngleInterpolator;
@@ -23,12 +19,12 @@ import com.jarvis.dragdropresearch.interpolators.ColorInterpolator;
 import com.jarvis.dragdropresearch.interpolators.RectangleInterpolator;
 import com.jarvis.dragdropresearch.interpolators.SpiralInterpolator;
 import com.jarvis.dragdropresearch.interpolators.TriangleInterpolator;
+import com.jarvis.dragdropresearch.utils.DrawingUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
@@ -251,7 +247,8 @@ public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
             // Draw the arc
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawArcShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true), paint);
+            DrawingUtils.drawArcShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true),
+                    paint);
         } else {
             colorInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
             shape.getAngleInterpolator()
@@ -260,44 +257,11 @@ public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
             // Draw the arc
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawArcShape(canvas, shape, getCommonShapeBoundingRect(page, shape, false), paint);
+            DrawingUtils.drawArcShape(canvas, shape, getCommonShapeBoundingRect(page, shape, false),
+                    paint);
         }
     }
 
-    private void drawArcShape(Canvas canvas, ArcShape shape, RectF boundingRect, Paint paint) {
-        AngleInterpolator angleInterpolator = shape.getAngleInterpolator();
-        float interpolatedAngle = angleInterpolator.getInterpolatedAngle();
-
-        if (shape.allowMultiColoredComponents()) {
-            // Number of degrees that each component will take up in the arc.
-            float angleFactor = 360.0f / shape.getMaxComponents();
-
-            // Total number of components that makeup the current interpolated angle.
-            int componentCount = (int)Math.floor(interpolatedAngle / angleFactor);
-
-            // Left-over degrees after dividing the interpolated angle by the angle factor.
-            float componentModulus = interpolatedAngle % angleFactor;
-
-            int[] componentColors = shape.getComponentColors();
-
-            float currentStartAngle = 0;
-            for (int i = 0; i < componentCount; i++) {
-                paint.setColor(componentColors[i % componentColors.length]);
-                canvas.drawArc(boundingRect, currentStartAngle,
-                        angleFactor, true, paint);
-                currentStartAngle += angleFactor;
-            }
-
-            if(componentModulus != 0){
-                paint.setColor(componentColors[componentCount % componentColors.length]);
-                canvas.drawArc(boundingRect, currentStartAngle,
-                        componentModulus, true, paint);
-            }
-        } else {
-            canvas.drawArc(boundingRect, 0,
-                    angleInterpolator.getInterpolatedAngle(), true, paint);
-        }
-    }
     //endregion
 
     //region Rectangle Drawing
@@ -309,7 +273,6 @@ public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
         paint.setStyle(Paint.Style.FILL);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
         ColorInterpolator colorInterpolator = shape.getColorInterpolator();
-        RectangleInterpolator rectangleInterpolator = shape.getRectangleInterpolator();
 
         if (pageShapeScrolledToTop(page, shape)) {
             // Means we've scrolled the current page to the top top of the visible part of the view.
@@ -318,84 +281,21 @@ public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
             // Draw the rectangle.
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawRectangleShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true),
-                    rectangleInterpolator, paint);
+            DrawingUtils.drawRectangleShape(canvas, shape,
+                    getCommonShapeBoundingRect(page, shape, true), paint);
         } else {
             colorInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
-            rectangleInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
+            shape.getRectangleInterpolator()
+                    .updateValue(getContentBoundsBottom() - page.getYPosition());
 
             // Draw the rectangle
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawRectangleShape(canvas, shape, getCommonShapeBoundingRect(page, shape, false),
-                    rectangleInterpolator, paint);
+            DrawingUtils.drawRectangleShape(canvas, shape,
+                    getCommonShapeBoundingRect(page, shape, false), paint);
         }
     }
 
-    private void drawRectangleShape(Canvas canvas, RectangleShape shape, RectF bounds,
-            RectangleInterpolator interpolator, Paint paint) {
-        float[] interpolatedDimensions = interpolator.getInterpolatedDimensions();
-        float interpolatedWidth =
-                interpolatedDimensions[RectangleInterpolator.INTERPOLATION_VALUES_WIDTH];
-        float interpolatedHeight =
-                interpolatedDimensions[RectangleInterpolator.INTERPOLATION_VALUES_HEIGHT];
-
-        if (!interpolator.isSymmetric()) {
-            float top = bounds.bottom - interpolatedHeight;
-            float bottom = bounds.bottom;
-            float left = bounds.left;
-            float right = left + interpolatedWidth;
-
-            canvas.drawRect(left, top, right, bottom, paint);
-        } else {
-            int[] componentColors = shape.getComponentColors();
-            boolean allowMultiColoredComponents = shape.allowMultiColoredComponents();
-
-            // Bottom Left Rectangle
-            float top = bounds.bottom - interpolatedHeight;
-            float bottom = bounds.bottom;
-            float left = bounds.left;
-            float right = left + interpolatedWidth;
-
-            if (allowMultiColoredComponents) {
-                paint.setColor(componentColors[0 % componentColors.length]);
-            }
-            canvas.drawRect(left, top, right, bottom, paint);
-
-            // Bottom Right Rectangle
-            top = bounds.bottom - interpolatedHeight;
-            bottom = bounds.bottom;
-            left = bounds.right - interpolatedWidth;
-            right = bounds.right;
-
-            if (allowMultiColoredComponents) {
-                paint.setColor(componentColors[1 % componentColors.length]);
-            }
-            canvas.drawRect(left, top, right, bottom, paint);
-
-            // Top Left Rectangle
-            top = bounds.top;
-            bottom = bounds.top + interpolatedHeight;
-            left = bounds.left;
-            right = bounds.left + interpolatedWidth;
-
-            if (allowMultiColoredComponents) {
-                paint.setColor(componentColors[2 % componentColors.length]);
-            }
-            canvas.drawRect(left, top, right, bottom, paint);
-
-            // Top Right Rectangle
-            top = bounds.top;
-            bottom = bounds.top + interpolatedHeight;
-            left = bounds.right - interpolatedWidth;
-            right = bounds.right;
-
-            if (allowMultiColoredComponents) {
-                paint.setColor(componentColors[3 % componentColors.length]);
-            }
-            canvas.drawRect(left, top, right, bottom, paint);
-        }
-    }
     //endregion
 
     //region Triangle Drawing
@@ -414,78 +314,19 @@ public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
             // Only drawing arcs for now
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawTriangleShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true),
-                    triangleInterpolator, paint);
+            DrawingUtils
+                    .drawTriangleShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true),
+                            paint);
         } else {
             colorInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
             triangleInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawTriangleShape(canvas, shape, getCommonShapeBoundingRect(page, shape, false),
-                    triangleInterpolator, paint);
+            DrawingUtils.drawTriangleShape(canvas, shape,
+                    getCommonShapeBoundingRect(page, shape, false), paint);
         }
     }
 
-    private void drawTriangleShape(Canvas canvas, TriangleShape shape, RectF bounds,
-            TriangleInterpolator triangleInterpolator, Paint paint) {
-        float baseInterpolation = triangleInterpolator
-                .getInterpolatedValues()[TriangleInterpolator.INTERPOLATION_VALUES_BASE];
-        float altitudeInterpolation = triangleInterpolator
-                .getInterpolatedValues()[TriangleInterpolator.INTERPOLATION_VALUES_ALTITUDE];
-        int[] colors = new int[] {paint.getColor(), paint.getColor()};
-
-        if (shape.allowMultiColoredComponents()
-                && triangleInterpolator.isSymmetric()) {
-            int[] componentColors = shape.getComponentColors();
-            colors[0] = shape.getComponentColors()[0 % componentColors.length];
-            colors[1] = shape.getComponentColors()[1 % componentColors.length];
-        }
-
-        // *** Construct the Left Triangle ** //
-
-        // Bottom left vertex
-        float bottomLeftX = bounds.left;
-        float bottomLeftY = bounds.bottom;
-
-        // Bottom right corner
-        float bottomRightX = bottomLeftX + baseInterpolation;
-        float bottomRightY = bounds.bottom;
-
-        //Top Vertex
-        float topX = bottomRightX;
-        float topY = bottomRightY - altitudeInterpolation;
-
-        Path leftTriangle = new Path();
-        leftTriangle.lineTo(bottomLeftX, bottomLeftY);
-        leftTriangle.lineTo(bottomRightX, bottomRightY);
-        leftTriangle.lineTo(topX, topY);
-        leftTriangle.lineTo(bottomLeftX, bottomLeftY);
-
-        paint.setColor(colors[0]);
-        canvas.drawPath(leftTriangle, paint);
-
-        if (triangleInterpolator.isSymmetric()) {
-            // *** Construct the Right Triangle ** //
-
-            bottomLeftX = bounds.right - baseInterpolation;
-            bottomLeftY = bounds.bottom;
-
-            bottomRightX = bounds.right;
-            bottomRightY = bounds.bottom;
-
-            topX = bottomLeftX;
-            topY = bottomRightY - altitudeInterpolation;
-
-            Path rightTriangle = new Path();
-            rightTriangle.lineTo(bottomLeftX, bottomLeftY);
-            rightTriangle.lineTo(bottomRightX, bottomRightY);
-            rightTriangle.lineTo(topX, topY);
-            rightTriangle.lineTo(bottomLeftX, bottomLeftY);
-
-            paint.setColor(colors[1]);
-            canvas.drawPath(rightTriangle, paint);
-        }
-    }
     //endregion
 
     //region Spiral Shape Drawing
@@ -497,141 +338,21 @@ public class FlashShapeView extends AbsCustomScrollingView<FlashShapePage> {
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
         paint.setStrokeWidth(10);
         ColorInterpolator colorInterpolator = shape.getColorInterpolator();
-        SpiralInterpolator spiralInterpolator = shape.getSpiralInterpolator();
 
         if (pageShapeScrolledToTop(page, shape)) {
-            // Means we've scrolled the current page to the top of the visible part of the view.
-            // Only drawing arcs for now
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawSpiralShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true),
-                    spiralInterpolator, paint);
+            DrawingUtils
+                    .drawSpiralShape(canvas, shape, getCommonShapeBoundingRect(page, shape, true),
+                            paint);
         } else {
             colorInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
-            spiralInterpolator.updateValue(getContentBoundsBottom() - page.getYPosition());
+            shape.getSpiralInterpolator().updateValue(getContentBoundsBottom() - page.getYPosition());
             paint.setColor(colorInterpolator.getInterpolatedShade());
 
-            drawSpiralShape(canvas, shape, getCommonShapeBoundingRect(page, shape, false),
-                    spiralInterpolator, paint);
-        }
-    }
-
-    private void drawSpiralShape(Canvas canvas, SpiralShape shape, RectF bounds,
-            SpiralInterpolator interpolator,
-            Paint paint) {
-        Path path = new Path();
-
-        List<SpiralArcDescriptor> arcDescriptors = new ArrayList<>();
-        List<SpiralSegment> segments = interpolator.getSegments();
-        if (segments == null || segments.isEmpty()) {
-            return;
-        }
-
-        // TODO: Populate array of random colors during initial setup to avoid excess
-        // object allocation in onDraw().
-
-        for (int i = 0; i < segments.size(); i++) {
-            final SpiralSegment segment = segments.get(i);
-
-            if (segment.getType() == SpiralSegment.Type.TOP) {
-                addTopSpiralSegment(path, arcDescriptors, bounds, segment);
-            } else {
-                addBottomSpiralSegment(path, arcDescriptors, bounds, segment);
-            }
-        }
-
-        final int defaultColor = paint.getColor();
-
-        final int[] segmentColors = shape.getComponentColors();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
-                shape.allowMultiColoredComponents()) {
-            for (int i = 0; i < arcDescriptors.size(); i++) {
-                SpiralArcDescriptor descriptor = arcDescriptors.get(i);
-
-                // Set segment color from poll of available segment colors.
-                int segmentColor = segmentColors[i % segmentColors.length];
-                if (shape.allowMultiColoredComponents() &&
-                        segmentColor != SpiralSegment.SEGMENT_COLOR_DEFAULT) {
-                    paint.setColor(segmentColor);
-                }
-                canvas.drawArc(descriptor.getLeft(), descriptor.getTop(), descriptor.getRight(),
-                        descriptor.getBottom(), descriptor.getStartAngle(),
-                        descriptor.getSweepAngle(), false, paint);
-
-                paint.setColor(defaultColor);
-            }
-        } else {
-            canvas.drawPath(path, paint);
-        }
-    }
-
-    private void addTopSpiralSegment(Path path,
-            List<SpiralArcDescriptor> arcDescriptors,
-            RectF bounds, SpiralSegment spiralSegment) {
-        float centerX = bounds.centerX();
-        float centerY = bounds.centerY();
-
-        float segmentBoundsLeft;
-        float segmentBoundsRight;
-        float segmentBoundsTop;
-        float segmentBoundsBottom;
-
-        if (path.isEmpty()) {
-            segmentBoundsLeft = centerX - spiralSegment.getWidth() / 2;
-            segmentBoundsRight = centerX + spiralSegment.getWidth() / 2;
-            segmentBoundsTop = centerY - spiralSegment.getHeight() / 2;
-            segmentBoundsBottom = centerY + spiralSegment.getHeight() / 2;
-        } else {
-            RectF pathBounds = new RectF();
-            path.computeBounds(pathBounds, true);
-
-            segmentBoundsLeft = pathBounds.left;
-            segmentBoundsRight = segmentBoundsLeft + spiralSegment.getWidth();
-            segmentBoundsTop = centerY - spiralSegment.getHeight() / 2;
-            segmentBoundsBottom = centerY + spiralSegment.getHeight() / 2;
-        }
-
-        arcDescriptors.add(new SpiralArcDescriptor(segmentBoundsLeft, segmentBoundsTop,
-                segmentBoundsBottom, segmentBoundsRight, 180,
-                180));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            path.addArc(segmentBoundsLeft, segmentBoundsTop, segmentBoundsRight,
-                    segmentBoundsBottom, 180, 180);
-        }
-    }
-
-    private void addBottomSpiralSegment(Path path,
-            List<SpiralArcDescriptor> arcDescriptors,
-            RectF bounds, SpiralSegment spiralSegment) {
-        float centerX = bounds.centerX();
-        float centerY = bounds.centerY();
-
-        float segmentBoundsLeft;
-        float segmentBoundsRight;
-        float segmentBoundsTop;
-        float segmentBoundsBottom;
-
-        if (path.isEmpty()) {
-            segmentBoundsLeft = centerX - spiralSegment.getWidth() / 2;
-            segmentBoundsRight = centerX + spiralSegment.getWidth() / 2;
-            segmentBoundsTop = centerY - spiralSegment.getHeight() / 2;
-            segmentBoundsBottom = centerY + spiralSegment.getHeight() / 2;
-        } else {
-            RectF pathBounds = new RectF();
-            path.computeBounds(pathBounds, true);
-
-            segmentBoundsLeft = pathBounds.right - spiralSegment.getWidth();
-            segmentBoundsRight = pathBounds.right;
-            segmentBoundsTop = centerY - spiralSegment.getHeight() / 2;
-            segmentBoundsBottom = centerY + spiralSegment.getHeight() / 2;
-        }
-
-        arcDescriptors.add(new SpiralArcDescriptor(segmentBoundsLeft, segmentBoundsTop,
-                segmentBoundsBottom, segmentBoundsRight, 0,
-                180));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            path.addArc(segmentBoundsLeft, segmentBoundsTop, segmentBoundsRight,
-                    segmentBoundsBottom, 0, 180);
+            DrawingUtils
+                    .drawSpiralShape(canvas, shape, getCommonShapeBoundingRect(page, shape, false),
+                            paint);
         }
     }
 
